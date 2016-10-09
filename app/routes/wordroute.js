00f6a0155173756c6df43b59bc2537f254d71cec
take-a-CHANCE <chancejd53@gmail.com>
2016-10-08 21:21:05 -0400

    Outline

--- app/routes/wordroute.js
+++ app/routes/wordroute.js
@@ -1,3 +1,9 @@
 module.exports = function(app) {
-  
+  // Need word checker route that checks the word given then routes to either
+  // good or bad route.
+
+  // Good route -> add word -> next player -> submit
+
+  // bad route -> remove player (check # players) -> next player if one avavilable
+  // -> submit
 }
