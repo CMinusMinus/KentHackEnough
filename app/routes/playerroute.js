f4b38066de1aae91c7d40274f3e44acd084272c7
take-a-CHANCE <chancejd53@gmail.com>
2016-10-09 01:26:41 -0400

    console ftw

--- app/routes/playerroute.js
+++ app/routes/playerroute.js
@@ -4,6 +4,7 @@ module.exports = function(app, mongoose) {
     var Player = mongoose.model('Player', require('../models/playerschema.js'))
     app.post('/player/create', function (req, res) {
         var name1 = req.body.name;
+        console.console.log(req.body);
 
         var newPlayer = new Player({
             name: name1,
