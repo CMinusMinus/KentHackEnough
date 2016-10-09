ed669ce21d57d218da38e3b7ae58753fc096382b
K Waters <waterskm2013@gmail.com>
2016-10-08 23:45:08 -0400

    Adding posting for player

--- app/routes/playerroute.js
+++ app/routes/playerroute.js
@@ -1,5 +1,26 @@
-var player = require('../controllers/playercontroller');
 
 module.exports = function(app) {
-    app.route('/players').post(player.create);
+    app.post('/player/create', function (req, res) {
+        var name = req.body.name;
+
+        var newTask = new Task({
+            name: name,
+            canPlay: true,
+            isTurn: false,
+            gamesWon: 0
+        });
+
+        newTask.save(function(err) {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        else
+            res.redirect("/lobby");
+        });
+    });
+
+  /*app.get('/task/create', function (req, res) {
+    res.redirect();
+  });*/
 };
