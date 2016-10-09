c4629b205d22123d16e53ca734932f24833060e3
take-a-CHANCE <chancejd53@gmail.com>
2016-10-08 21:39:36 -0400

    Maybe possibly made add word?

--- app/routes/wordroute.js
+++ app/routes/wordroute.js
@@ -1,3 +1,5 @@
+var Game = require('../models/Game')
+
 module.exports = function(app) {
   // Need word checker route that checks the word given then routes to either
   // good or bad route.
@@ -6,4 +8,9 @@ module.exports = function(app) {
 
   // bad route -> remove player (check # players) -> next player if one avavilable
   // -> submit
+
+  // add word
+  // throwing shit at the wall here
+  app.post('loc',$word);
+
 }
