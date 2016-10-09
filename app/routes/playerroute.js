3c1bf1ebbce16f3664e7e46d6dbfa5a9d83034b7
take-a-CHANCE <chancejd53@gmail.com>
2016-10-09 01:42:12 -0400

    change console output

--- app/routes/playerroute.js
+++ app/routes/playerroute.js
@@ -5,7 +5,7 @@ module.exports = function(app, mongoose) {
     app.post('/player/create', function (req, res) {
         var name1 = req.body.name;
         console.log(req.body);
-        console.log(req.body.name);
+        console.log(name1);
 
         var newPlayer = new Player({
             name: name1,
