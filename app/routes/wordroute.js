4b0f5f80f65afd54c3ef8a6ddc6527261bc9624e
take-a-CHANCE <chancejd53@gmail.com>
2016-10-09 04:38:54 -0400

    Made method for making word in DB

--- app/routes/wordroute.js
+++ app/routes/wordroute.js
@@ -1,6 +1,8 @@
-var Game = require('../models/Game')
+var Game = require('../models/gameschema.js');
+var GameRoute = require('gameroute');
 
-module.exports = function(app) {
+
+module.exports = function(app,mongoose) {
   // Need word checker route that checks the word given then routes to either
   // good or bad route.
 
@@ -9,9 +11,18 @@ module.exports = function(app) {
   // bad route -> remove player (check # players) -> next player if one avavilable
   // -> submit
 
+
   // add word
   // throwing shit at the wall here
-  app.post('/game/words',req.body.text);
-  
+  app.post('/game/words',function(req,res){
+    var query = Game.where({accessCode: req.body.accessCode})
+    query.findOne(function(err,Game) {
+        if(err) return next(err);
+        if(Game) {
+          Game.words.push(req.body.word);
+        }
+    });
+  });
+
 
 }
