4e97d0e83208be99be784fff327e75197fb78890
take-a-CHANCE <chancejd53@gmail.com>
2016-10-09 01:44:07 -0400

    fuck new

--- app/models/playerschema.js
+++ app/models/playerschema.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose'),
     PlayerSchema = mongoose.Schema;
 
-var PlayerSchema = new mongoose.Schema({
+var PlayerSchema = mongoose.Schema({
     name: { type: String },
     canPlay: { type: Boolean },
     isTurn: { type: Boolean },
