a2025ec6b99b49a114a044dd28800d4b7cd06ef1
K Waters <waterskm2013@gmail.com>
2016-10-08 22:31:20 -0400

    parentheses are important

--- app/controllers/gamecontroller.js
+++ app/controllers/gamecontroller.js
@@ -2,7 +2,7 @@ var Game = require('mongoose').model('GameSchema');
 
 exports.create = function(req, res, next) {
     var game = new Game(req.body);
-    game.save(function(err)) {
+    game.save(function(err) {
         if (err) {
             return next(err);
         }
--- app/controllers/playercontroller.js
+++ app/controllers/playercontroller.js
@@ -2,7 +2,7 @@ var User = require('mongoose').model('PlayerSchema');
 
 exports.create = function(req, res, next) {
     var player = new User(req.body);
-    user.save(function(err)) {
+    user.save(function(err) {
         if (err) {
             return next(err);
         }
