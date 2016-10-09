cadb73113481b5942389bff5d92d0db597e14403
take-a-CHANCE <chancejd53@gmail.com>
2016-10-09 03:39:58 -0400

    fucking merging

--- app/models/gameschema.js
+++ app/models/gameschema.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var mongoose = require('mongoose'),
 
     GameSchema = mongoose.Schema;
@@ -13,16 +12,3 @@ var GameSchema = mongoose.Schema({
 var Game = mongoose.model('Game', GameSchema);
 
 module.exports = Game;
-=======
-var mongoose = require('playerschema'),
-    GameSchema = mongoose.Schema;
-
-var GameSchema = new Schema({
-    accessCode: Number,
-    wordsPlayed: Array,
-    players: Array,
-    turnIndex: Number,
-});
-
-mongoose.model('Game', GameSchema);
->>>>>>> Angular
